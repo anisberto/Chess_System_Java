c99f7711917266748c4adc14f32ed17d2e7da3f6
Anisberto <anisbertoos@gmail.com>
2020-04-09 17:59:45 -0300

    Created class Position and boardgame

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,8 +1,11 @@
 package aplication;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Started Project! ");
+		Position posicao = new Position(15, 2);
+		System.out.println(posicao);
 	}
 }
---
+++ src/boardgame/Position.java
@@ -0,0 +1,42 @@
+package boardgame;
+
+public class Position {
+	private Integer row = 0;
+	private Integer column = 0;
+
+	public Position() {
+
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	public Position(Position object) {
+		this.row = object.row;
+		this.column = object.column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+
+}
