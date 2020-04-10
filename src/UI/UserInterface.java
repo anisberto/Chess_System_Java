b143a18cadc07846e5ecb80ec2e2f3f9d92fae2c
Anisberto <anisbertoos@gmail.com>
2020-04-10 16:44:11 -0300

    ChessException and the Position Chess on the Board

--- src/UI/UserInterface.java
+++ src/UI/UserInterface.java
@@ -5,13 +5,13 @@ import chess.ChessPiece;
 public class UserInterface {
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print("|" + (8 - i) + "| ");
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  A B C D E F G H");
+		System.out.println("    A B C D E F G H ");
 	}
 
 	// Metodo para imprimir uma unica peça do tabuleiro
@@ -19,7 +19,7 @@ public class UserInterface {
 		if (piece != null) {
 			System.out.print(piece);
 		} else {
-			System.out.print("-");
+			System.out.print("*");
 		}
 		System.out.print(" ");
 	}
--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -7,7 +7,6 @@ public class Program {
 
 	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
-		
 		UserInterface.printBoard(chessMatch.getPieces());
 		
 	}
