aad87b328fa29029403b099dfa4ea21c1c6ec562
Anisberto Reis <anisbertoos@gmail.com>
2020-04-12 04:02:54 -0300

    Implementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,14 +16,57 @@ public class King extends ChessPiece {
 		return "K";
 	}
 
+	// Metodos Auxiliar
+	private boolean canMove(Position position) {
+		ChessPiece pieceThisPosition = (ChessPiece) getBoard().piece(position);
+		return pieceThisPosition == null || pieceThisPosition.getColor() != getColor();
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] possibleMatMove = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		for (int i = 0; i < possibleMatMove.length; i++) {
-			for (int j = 0; j < possibleMatMove[i].length; j++) {
+		Position positionAux = new Position(0, 0);
 
-			}
-		}
+		// Posição superior
+		positionAux.setValuePositions(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			possibleMatMove[positionAux.getRow()][positionAux.getColumn()] = true;
+
+		// Posição Inferior
+		positionAux.setValuePositions(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			possibleMatMove[positionAux.getRow()][positionAux.getColumn()] = true;
+
+		// Posição esquerdo
+		positionAux.setValuePositions(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			possibleMatMove[positionAux.getRow()][positionAux.getColumn()] = true;
+
+		// Posição direito
+		positionAux.setValuePositions(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			possibleMatMove[positionAux.getRow()][positionAux.getColumn()] = true;
+
+		// Posição NW
+		positionAux.setValuePositions(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			possibleMatMove[positionAux.getRow()][positionAux.getColumn()] = true;
+
+		// Posição NE
+		positionAux.setValuePositions(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			possibleMatMove[positionAux.getRow()][positionAux.getColumn()] = true;
+
+		// Posição SW
+		positionAux.setValuePositions(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			possibleMatMove[positionAux.getRow()][positionAux.getColumn()] = true;
+
+		// Posição SE
+		positionAux.setValuePositions(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(positionAux) && canMove(positionAux))
+			possibleMatMove[positionAux.getRow()][positionAux.getColumn()] = true;
+
 		return possibleMatMove;
 	}
 
